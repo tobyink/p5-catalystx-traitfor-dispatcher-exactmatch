1445ab988816ff75bf3924e382b6b4f552a3e9e6
Toby Inkster <mail@tobyinkster.co.uk>
2014-09-11 10:47:16 +0100

    copyright year
    --HG--
    branch : CatalystX-TraitFor-Dispatcher-ExactMatch

--- lib/CatalystX/TraitFor/Dispatcher/ExactMatch.pm
+++ lib/CatalystX/TraitFor/Dispatcher/ExactMatch.pm
@@ -104,7 +104,7 @@ Toby Inkster E<lt>tobyink@cpan.orgE<gt>.
 
 =head1 COPYRIGHT AND LICENCE
 
-This software is copyright (c) 2011 by Toby Inkster.
+This software is copyright (c) 2011, 2014 by Toby Inkster.
 
 This is free software; you can redistribute it and/or modify it under
 the same terms as the Perl 5 programming language system itself.
